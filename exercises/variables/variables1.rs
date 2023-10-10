798cc816760b362446f2eb0306fcd5c128f89500
Your Name <you@example.com>
2023-10-10 23:57:38 +0800

    update

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -5,9 +5,10 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    let x = 5;
+    //let y = {let z = 7};
+    let y = x;
     println!("x has the value {}", x);
 }
