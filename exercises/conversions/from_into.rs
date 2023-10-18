1dbcaa9e4a6d3d4456b6aa5083c778f98d71ad95
Your Name <you@example.com>
2023-10-18 10:23:00 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Person::default();
+        }
+        let name = v[0];
+        if name.len() == 0 {
+            return Person::default();
+        }
+        if let Ok(age) = v[1].parse::<usize>() {
+            return Person {
+                name: name.to_string(),
+                age,
+            }
+        }
+        Person::default()
     }
 }
 
