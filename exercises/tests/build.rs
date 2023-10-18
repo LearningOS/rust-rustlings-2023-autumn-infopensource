c6d10c1b4a18b349d2def3ef1fd572accf624e60
Your Name <you@example.com>
2023-10-18 21:37:31 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -6,19 +6,28 @@ fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
+
+
+// In this exercise, we look for an environment variable and expect it to
+// fall in a range. You can look into the testcase to find out the details.
+
+    
+    
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
     let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
+        "rustc-env=TEST_FOO={}",
         timestamp
     );
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+
+    
     println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
